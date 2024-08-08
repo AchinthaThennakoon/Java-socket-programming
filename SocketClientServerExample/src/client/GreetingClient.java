e6add8344fbc920900939bf1cbd78d62bbda519d
Achintha Thennakoon <achinthathennakoon97@gmail.com>
2024-08-08 14:13:10 +0530

    initial commit

---
+++ SocketClientServerExample/src/client/GreetingClient.java
@@ -0,0 +1,30 @@
+package client;
+
+import java.io.*;
+import java.net.Socket;
+
+public class GreetingClient {
+    public static void main(String[] args) {
+        String serverName = args[0];
+        int port = Integer.parseInt(args[1]);
+
+        try {
+            System.out.println("Connecting to " + serverName + " on port " + port);
+            Socket client = new Socket(serverName, port);
+
+            System.out.println("Just connected to :"+ client.getRemoteSocketAddress());
+
+            OutputStream outToServer = client.getOutputStream();
+            DataOutputStream out = new DataOutputStream(outToServer);
+            out.writeUTF("hello from :" + client.getLocalAddress());
+
+            InputStream inFromServer = client.getInputStream();
+            DataInputStream in  = new DataInputStream(inFromServer);
+            System.out.println("Server says"+in.readUTF());
+
+        }catch (IOException e){
+
+        }
+
+    }
+}
