64c2a39493685297ad93cabd78f4557fa5967e67
Achintha Thennakoon <achinthathennakoon97@gmail.com>
2024-08-15 12:30:25 +0530

    create read-wtrite program

--- read-write-both/ClientServerApp/src/Main.java
+++ read-write-both/ClientServerApp/src/Main.java
@@ -1,5 +1,36 @@
+import java.io.*;
+import java.net.Socket;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            Socket s = new Socket("localhost",6666);
+
+            String input = "";
+            String output = "";
+
+            DataInputStream inputStream = new DataInputStream(s.getInputStream());
+            DataOutputStream outputStream = new DataOutputStream(s.getOutputStream());
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+            while (!input.equals("stop")){
+                output = br.readLine();
+                outputStream.writeUTF(output);
+                outputStream.flush();
+
+                input = inputStream.readUTF();
+                System.out.println("server :"+ input);
+
+            }
+
+            inputStream.close();
+            outputStream.close();
+            s.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
--- read-write-both/MyServerApp/src/Main.java
+++ read-write-both/MyServerApp/src/Main.java
@@ -1,5 +1,40 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        try {
+            ServerSocket ss = new ServerSocket(6666);
+            Socket s = ss.accept();
+
+            String input = "";
+            String output = "";
+
+            DataInputStream inputStream = new DataInputStream(s.getInputStream());
+            DataOutputStream outputStream = new DataOutputStream(s.getOutputStream());
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+            while (!input.equals("stop")){
+                input = inputStream.readUTF();
+                System.out.println("client :"+ input);
+
+                output = br.readLine();
+                outputStream.writeUTF(output);
+
+                outputStream.flush();
+            }
+
+            inputStream.close();
+            outputStream.close();
+            s.close();
+            ss.close();
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
     }
 }
